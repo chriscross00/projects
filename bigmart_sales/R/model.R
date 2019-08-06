a699655511b02783d8888f68a57e8860397fccc0
chriscross00 <christopher.chan2014@gmail.com>
2019-08-05 23:52:42 -0700

    train/test/split function

--- bigmart_sales/R/model.R
+++ bigmart_sales/R/model.R
@@ -2,3 +2,16 @@
 # 1. Split data 70-30
 # 2. Make model
 # 3. Validate model
+
+create_train_test_split <- function(data, size = 0.7) {
+  set.seed(50)
+  
+  train_ind <- sample(nrow(data), size = floor(size*nrow(data)))
+  train <- data[train_ind, ]
+  test <- data[-train_ind, ]
+  train_test_data <- list(train=train, test=test)
+  
+  return(train_test_data)
+}
+
+
