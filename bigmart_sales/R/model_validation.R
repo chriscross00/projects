bca225ff32c37e0e27a7a431f1c6d4d7814a0648
Christopher Chan <christopher.chan2014@gmail.com>
2019-09-16 23:05:28 -0400

    even more reading

--- bigmart_sales/R/model_validation.R
+++ bigmart_sales/R/model_validation.R
@@ -1,4 +1,5 @@
 # Read this to understand what measure of feat importance to use:
 # https://stats.stackexchange.com/questions/197827/how-to-interpret-mean-decrease-in-accuracy-and-mean-decrease-gini-in-random-fore
 # r-bloggers.com/random-forest-variable-importance/
+# https://towardsdatascience.com/explaining-feature-importance-by-example-of-a-random-forest-d9166011959e
 
